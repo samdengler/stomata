794eb855c665f3468fd9a45c9a46318125a18023
Badrinath Janakiraman <Thoughtworker@arvo-2.local>
2013-07-16 08:56:13 +0530

    Add new test

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -7,5 +7,11 @@ class PostTest < ActiveSupport::TestCase
     assert_equal 1, Post.count
     assert_equal 'trivial', p.content
   end
-  
+
+  test "copy self over" do
+    FileUtils.mkdir_p("artifats/interesting")
+    File.open("artifats/interesting/post_test.rb", 'w') do |f|
+      f << File.readlines(File.expand_path(__FILE__)).join("\n")
+    end
+  end
 end
